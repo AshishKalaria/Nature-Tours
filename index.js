7103710e1972614aa4a7e58dd08964ebb51e7f02
Ashish Kalaria <131858574+AshishKalaria@users.noreply.github.com>
2023-12-20 19:35:58 +0530

    basic index file

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const tours = fs.readFileSync();
+
+app.get("/api/v1/tours", (req, res) => {});
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
